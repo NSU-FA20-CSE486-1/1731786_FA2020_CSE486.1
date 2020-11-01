33b4e3f2aec61fc9bb9cf5d7dd9ca2ae617c9bff
Salma-Abdul-Hai <salma.hai@northsouth.edu>
2020-11-02 04:21:58 +0600

    Added lab1 code to the lab folder
     Transfered Lab 01 code folder submitted for Lab 01 inside the "Lab 01 - Oct 29 2020 Thursday" folder.

---
+++ Assignments/Lab/Lab 01 - Oct 29 2020 Thursday/Lab1InClassCodeChallenge/app/src/main/java/com/salmaabdulhai/lab1inclasscodechallenge/MainActivity.java
@@ -0,0 +1,17 @@
+package com.salmaabdulhai.lab1inclasscodechallenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.e("MainActivity OnCreate", "Forgot Birthday Exception");
+    }
+}
