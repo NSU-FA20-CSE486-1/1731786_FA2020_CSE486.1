4020eb14448701ddcf750d20ba5ecb44b542148d
Salma-Abdul-Hai <salma.hai@northsouth.edu>
2020-12-16 13:51:46 +0600

    Midterm Assignment Android Application #43 User Interface
    Created the layout for midterm assignment android app.

---
+++ Assignments/Midterm_Assignment_Android_App/app/src/main/java/com/salmaabdulhai/encryptedtextmessageapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.salmaabdulhai.encryptedtextmessageapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
