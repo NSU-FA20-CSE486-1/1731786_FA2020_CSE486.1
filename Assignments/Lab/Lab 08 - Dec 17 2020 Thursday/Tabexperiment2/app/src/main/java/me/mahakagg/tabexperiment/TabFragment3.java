1509cc063c5657b95a47c6e844e725e8c06b0e0b
Salma-Abdul-Hai <salma.hai@northsouth.edu>
2020-12-19 17:28:56 +0600

    ASSIGNMNET 08: Created another tab option in an existing app and added article for each tab.
    ASSIGNMNET 08: Created another tab option in an existing app and added article for each tab.

---
+++ Assignments/Lab/Lab 08 - Dec 17 2020 Thursday/Tabexperiment2/app/src/main/java/me/mahakagg/tabexperiment/TabFragment3.java
@@ -0,0 +1,32 @@
+package me.mahakagg.tabexperiment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TabFragment3 extends Fragment {
+    //Button b3;
+
+    public TabFragment3() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_tab_fragment3, container, false);
+        //b3 = v.findViewById(R.id.button_tab3);
+        return v;
+    }
+
+}
---
+++ Assignments/Lab/Lab 08 - Dec 17 2020 Thursday/Tabexperiment2/app/src/main/java/me/mahakagg/tabexperiment/TabFragment4.java
@@ -0,0 +1,32 @@
+package me.mahakagg.tabexperiment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TabFragment4 extends Fragment {
+    //Button b4;
+
+    public TabFragment4() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_tab_fragment4, container, false);
+        //b4 = v.findViewById(R.id.button_tab4);
+        return v;
+    }
+
+}
