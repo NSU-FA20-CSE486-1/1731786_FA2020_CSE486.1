cf5dfbf66e3b5fe89b6726f45332426772dbfa4d
Salma-Abdul-Hai <salma.hai@northsouth.edu>
2020-12-27 23:27:46 +0600

    #47 Created the layout for ordered items and created adapters and model and implemneted the class methods in the activity

--- Android Project/Project/Khadok/app/src/main/java/com/salmaabdulhai/khadok/Adapters/CustomerOrderAdapter.java
+++ Android Project/Project/Khadok/app/src/main/java/com/salmaabdulhai/khadok/Adapters/CustomerOrderAdapter.java
@@ -1,6 +1,69 @@
 package com.salmaabdulhai.khadok.Adapters;
 
-public class CustomerOrderAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-    
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.salmaabdulhai.khadok.Models.CustomerOrderModel;
+import com.salmaabdulhai.khadok.R;
+
+import java.util.ArrayList;
+
+public class CustomerOrderAdapter extends RecyclerView.Adapter<CustomerOrderAdapter.viewHolder> {
+
+
+    ArrayList <CustomerOrderModel> list;
+    Context context;
+
+    public CustomerOrderAdapter(ArrayList<CustomerOrderModel> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(context).inflate(R.layout.sample_customer_orders, parent, false);
+
+        return new viewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull viewHolder holder, int position) {
+        final CustomerOrderModel model = list.get(position);
+        holder.orderedFoodImage.setImageResource(model.getOrderedImage());
+        holder.orderedPrice.setText(model.getPrice());
+        holder.orderedFoodName.setText(model.getOrderedFood());
+        holder.orderedNumber.setText(model.getOrderNumber());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class viewHolder extends RecyclerView.ViewHolder {
+
+        ImageView orderedFoodImage;
+        TextView orderedPrice, orderedFoodName, orderedNumber;
+
+        public viewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            orderedFoodImage = (ImageView) itemView.findViewById(R.id.orderedImageView);
+            orderedPrice = (TextView) itemView.findViewById(R.id.orderedFoodPrice);
+            orderedNumber = (TextView) itemView.findViewById(R.id.orderID);
+            orderedFoodName = (TextView) itemView.findViewById(R.id.orderedFoodname);
+        }
+
+
+    }
 }
--- Android Project/Project/Khadok/app/src/main/java/com/salmaabdulhai/khadok/CustomerOrderActivity.java
+++ Android Project/Project/Khadok/app/src/main/java/com/salmaabdulhai/khadok/CustomerOrderActivity.java
@@ -1,14 +1,43 @@
 package com.salmaabdulhai.khadok;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
 
+import com.salmaabdulhai.khadok.Adapters.CustomerOrderAdapter;
+import com.salmaabdulhai.khadok.Adapters.MainAdapter;
+import com.salmaabdulhai.khadok.Models.CustomerOrderModel;
+import com.salmaabdulhai.khadok.Models.MainModel;
+import com.salmaabdulhai.khadok.databinding.ActivityCustomerOrderBinding;
+import com.salmaabdulhai.khadok.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+
 public class CustomerOrderActivity extends AppCompatActivity {
+
+    ActivityCustomerOrderBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_customer_order);
+        binding = ActivityCustomerOrderBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        ArrayList<CustomerOrderModel> list = new ArrayList<>();
+        list.add(new CustomerOrderModel(R.drawable.kacchi_biryani, "250", "Kacchi Biryani", "k14788523"));
+        list.add(new CustomerOrderModel(R.drawable.kacchi_biryani, "250", "Kacchi Biryani", "k14788523"));
+        list.add(new CustomerOrderModel(R.drawable.kacchi_biryani, "250", "Kacchi Biryani", "k14788523"));
+        list.add(new CustomerOrderModel(R.drawable.kacchi_biryani, "250", "Kacchi Biryani", "k14788523"));
+        list.add(new CustomerOrderModel(R.drawable.kacchi_biryani, "250", "Kacchi Biryani", "k14788523"));
+        list.add(new CustomerOrderModel(R.drawable.kacchi_biryani, "250", "Kacchi Biryani", "k14788523"));
+        list.add(new CustomerOrderModel(R.drawable.kacchi_biryani, "250", "Kacchi Biryani", "k14788523"));
+        list.add(new CustomerOrderModel(R.drawable.kacchi_biryani, "250", "Kacchi Biryani", "k14788523"));
+        CustomerOrderAdapter adapter = new CustomerOrderAdapter(list, this);
+        binding.orderedRecyclerView.setAdapter(adapter);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        binding.orderedRecyclerView.setLayoutManager(layoutManager);
+
     }
 }
