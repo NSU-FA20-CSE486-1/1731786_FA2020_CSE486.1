66c40ca2f9a5cb27657823b87da264fea05be776
Salma-Abdul-Hai <salma.hai@northsouth.edu>
2020-12-28 02:59:40 +0600

    #59  Created the layout for order food items detail for customer

---
+++ Android Project/Project/Khadok/app/src/main/java/com/salmaabdulhai/khadok/customer_order_detail.java
@@ -0,0 +1,14 @@
+package com.salmaabdulhai.khadok;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class customer_order_detail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_order_detail);
+    }
+}
