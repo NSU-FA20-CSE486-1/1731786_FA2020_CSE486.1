7c51a0a3423571647a3cd2d643da1185b60e9104
Salma-Abdul-Hai <salma.hai@northsouth.edu>
2021-01-04 02:35:16 +0600

    #48 using the google API, set a working text fieldfor places prediction as the user types in the location

--- Android Project/Project/Khadok/app/src/main/java/com/salmaabdulhai/khadok/googleAddressActivity.java
+++ Android Project/Project/Khadok/app/src/main/java/com/salmaabdulhai/khadok/googleAddressActivity.java
@@ -3,6 +3,17 @@ package com.salmaabdulhai.khadok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.net.PlacesClient;
+
+import java.util.Arrays;
 
 public class googleAddressActivity extends AppCompatActivity {
 
@@ -10,5 +21,12 @@ public class googleAddressActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_address);
+
+        Places.initialize(getApplicationContext(), "AIzaSyDw2zKNulw3Lex1adaCSRBOGwTBFURtzns");
+        PlacesClient placesClient = Places.createClient(this);
+
+
+
+
     }
 }
