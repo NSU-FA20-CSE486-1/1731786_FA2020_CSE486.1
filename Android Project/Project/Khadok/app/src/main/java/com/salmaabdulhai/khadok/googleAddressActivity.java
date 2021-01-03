7dcc494e543929f3fd0770adb83b1f6a23691ecc
Salma-Abdul-Hai <salma.hai@northsouth.edu>
2021-01-04 03:56:34 +0600

    #48 Tried to set the google api for the customers exact address

--- Android Project/Project/Khadok/app/src/main/java/com/salmaabdulhai/khadok/googleAddressActivity.java
+++ Android Project/Project/Khadok/app/src/main/java/com/salmaabdulhai/khadok/googleAddressActivity.java
@@ -4,18 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Toast;
-
 import com.google.android.gms.common.api.Status;
-import com.google.android.gms.location.places.Place;
-import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
-import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.RectangularBounds;
+import com.google.android.libraries.places.api.model.TypeFilter;
 import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
 
 public class googleAddressActivity extends AppCompatActivity {
+
+
+    private static final String TAG = "googleAddressActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +30,41 @@ public class googleAddressActivity extends AppCompatActivity {
 
         Places.initialize(getApplicationContext(), "AIzaSyDw2zKNulw3Lex1adaCSRBOGwTBFURtzns");
         PlacesClient placesClient = Places.createClient(this);
+
+        // Initialize the AutocompleteSupportFragment.
+        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
+                getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+
+        autocompleteFragment.setTypeFilter(TypeFilter.ADDRESS);
+        autocompleteFragment.setLocationBias(RectangularBounds.newInstance(
+                new LatLng(-33.880490, 151.184363),
+                new LatLng(-33.858754, 151.229596 )));
+
+        autocompleteFragment.setCountries("IN");
+
+        // Specify the types of place data to return.
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME));
+
+        // Set up a PlaceSelectionListener to handle the response.
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+
+
+            @Override
+            public void onPlaceSelected(@NotNull Place place) {
+                // TODO: Get info about the selected place.
+                Log.i(TAG, "Place: " + place.getName() + ", " + place.getId());
+            }
+
+
+            @Override
+            public void onError(@NotNull Status status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
+
+
+
 
 
 
