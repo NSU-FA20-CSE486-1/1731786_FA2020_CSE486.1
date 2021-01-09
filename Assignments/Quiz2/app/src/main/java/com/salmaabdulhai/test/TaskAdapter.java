808252084081d41e1f9c2d15aea804c741cf57dc
Salma-Abdul-Hai <salma.hai@northsouth.edu>
2021-01-09 19:26:22 +0600

    #55 Onitem click lisenter for pop up window

--- Assignments/Quiz2/app/src/main/java/com/salmaabdulhai/test/TaskAdapter.java
+++ Assignments/Quiz2/app/src/main/java/com/salmaabdulhai/test/TaskAdapter.java
@@ -1,11 +1,13 @@
 package com.salmaabdulhai.test;
 
 
+import android.app.Dialog;
 import android.content.Context;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -17,6 +19,7 @@ import java.util.List;
 public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.Holder> {
     private Context context;
     private List<TaskModel> taskModelList;
+    Dialog myDialogue;
 
     public TaskAdapter(Context context, List<TaskModel> taskModelList) {
         this.context = context;
@@ -32,8 +35,26 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.Holder> {
     @NonNull
     @Override
     public Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new Holder(LayoutInflater.from(context)
-                .inflate(R.layout.english_words, parent, false));
+        View v;
+        v = LayoutInflater.from(context).inflate(R.layout.english_words, parent, false);
+        Holder vHolder= new Holder(v);
+        myDialogue =new Dialog(context);
+        myDialogue.setContentView(R.layout.dialog_message);
+
+
+        vHolder.popup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                TextView popMess = (TextView) myDialogue.findViewById(R.id.popupMessage);
+                popMess.setText(taskModelList.get(vHolder.getAdapterPosition()).getTaskName());
+                myDialogue.show();
+
+
+            }
+        });
+
+        return vHolder;
     }
 
     @Override
@@ -47,10 +68,12 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.Holder> {
     }
 
     public class Holder extends RecyclerView.ViewHolder {
-        TextView txtTaskName, txtTaskAddTime;
+        TextView txtTaskName;
+        LinearLayout popup;
         public Holder(@NonNull View itemView) {
             super(itemView);
             txtTaskName = itemView.findViewById(R.id.txt_task_name);
+            popup = (LinearLayout) itemView.findViewById(R.id.popupid);
         }
     }
 
