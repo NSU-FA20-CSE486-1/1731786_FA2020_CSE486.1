93fa4912f0dc5996561714ec76cb05d4bb8e2468
Salma-Abdul-Hai <salma.hai@northsouth.edu>
2021-01-15 01:16:04 +0600

    #51 created the fragment class and layout for chef dashboard and add food items and set onclick listeners

--- Android Project/Project/Khadok/app/src/main/java/com/salmaabdulhai/khadok/chef_dashboard.java
+++ Android Project/Project/Khadok/app/src/main/java/com/salmaabdulhai/khadok/chef_dashboard.java
@@ -1,14 +1,37 @@
 package com.salmaabdulhai.khadok;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
-public class chef_dashboard extends AppCompatActivity {
+public class chef_dashboard extends Fragment{
 
+    Button addFoodBtn;
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chef_dashboard);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.chef_dashboard_frag, container,false);
+
+        addFoodBtn = view.findViewById(R.id.dash_add_item);
+        addFoodBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new Fragment();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container, new AddFoodItemsFrag());
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+        return view;
     }
 }
--- Android Project/Project/Khadok/app/src/main/java/com/salmaabdulhai/khadok/chef_reg_Frag.java
+++ Android Project/Project/Khadok/app/src/main/java/com/salmaabdulhai/khadok/chef_reg_Frag.java
@@ -4,15 +4,33 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class chef_reg_Frag extends Fragment {
+
+    Button signup_chef;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.chef_registration_fragment, container, false);
+        View view = inflater.inflate(R.layout.chef_registration_fragment, container,false);
+        signup_chef = view.findViewById(R.id.ChefSignup_btn);
+        signup_chef.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new Fragment();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container, new chef_dashboard());
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+        return view;
     }
 }
