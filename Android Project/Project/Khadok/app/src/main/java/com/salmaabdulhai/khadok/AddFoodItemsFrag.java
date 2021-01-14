1ba6398fc0df21abd97780622dadb0f5409f7461
Salma-Abdul-Hai <salma.hai@northsouth.edu>
2021-01-15 01:47:32 +0600

    #51 Created two fragments and recylcer view to hold the added/offered food items of the chef and display it on chefs dashboard

--- Android Project/Project/Khadok/app/src/main/java/com/salmaabdulhai/khadok/AddFoodItemsFrag.java
+++ Android Project/Project/Khadok/app/src/main/java/com/salmaabdulhai/khadok/AddFoodItemsFrag.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -16,7 +18,15 @@ public class AddFoodItemsFrag extends Fragment{
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.home_fragment, container,false);
+        View view = inflater.inflate(R.layout.add_food_items_frag, container,false);
+        setHasOptionsMenu(true);
         return view;
+    }
+
+    // to remove the menu
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        menu.clear();
     }
 }
--- Android Project/Project/Khadok/app/src/main/java/com/salmaabdulhai/khadok/orderDetailFrag.java
+++ Android Project/Project/Khadok/app/src/main/java/com/salmaabdulhai/khadok/orderDetailFrag.java
@@ -15,7 +15,7 @@ public class orderDetailFrag extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
 
-        View view = inflater.inflate(R.layout.activity_customer_order_detail, container,false);
+        View view = inflater.inflate(R.layout.customer_order_detail_frag, container,false);
         return view;
     }
 }
